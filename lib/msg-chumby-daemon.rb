7f4296f9b60dc937077e14311d77c157a7fb81f6
Mathias Dalheimer <md@gonium.net>
2010-05-31 16:26:54 +0200

    renamed reading importer

--- lib/msg-chumby-daemon.rb
+++ lib/msg-chumby-daemon.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 require 'flukso'
 require 'lib/msg-chumby-daemon/http-xml-server.rb'
 require 'lib/msg-chumby-daemon/reading-cache.rb'
-require 'lib/msg-chumby-daemon/demo-reading-importer.rb'
+require 'lib/msg-chumby-daemon/reading-importer.rb'
 
