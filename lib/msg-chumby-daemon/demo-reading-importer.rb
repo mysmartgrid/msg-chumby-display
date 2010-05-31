0070cead9489418d43ef69178ea38823f3e1d154
Mathias Dalheimer <md@gonium.net>
2010-05-31 16:24:07 +0200

    random UTCReadings can be requested.

--- lib/msg-chumby-daemon/demo-reading-importer.rb
+++ lib/msg-chumby-daemon/demo-reading-importer.rb
@@ -1,4 +1,8 @@
+require 'rubygems'
 require 'flukso'
+if __FILE__ == $0
+  require File.join(File.dirname(__FILE__), 'reading-cache.rb');
+end
 
 module MSG_Chumby
   class DemoReadingImporter
@@ -6,27 +10,20 @@ module MSG_Chumby
       @reading_cache=reading_cache
       @max_reading=max_reading
     end
-    def start
-      @thread=Thread.new do
-        run();
-      end
-    end
-    def stop
-      @thread.stop
-    end
-    def join
-      @thread.join
-    end
-    def run
-      loop do
-        generate_value();
-        sleep(3);
-      end
-    end
-    def generate_value
-      value=rand(max_reading)
-      reading=UTCReading.new(Time.now, value)
-      @reading_cache.update_last_reading(reading);
+    def doWork
+        loop do
+          value=rand(@max_reading)
+          reading=Flukso::UTCReading.new(Time.now.to_i, value)
+          #puts "generated random reading #{reading}"
+          @reading_cache.update_last_reading(reading);
+          sleep(3);
+        end
     end
   end
+end
+
+if __FILE__ == $0
+  reading_cache=MSG_Chumby::Reading_Cache.new();
+  demo_importer=MSG_Chumby::DemoReadingImporter.new(reading_cache, 400);
+  demo_importer.doWork();
 end
