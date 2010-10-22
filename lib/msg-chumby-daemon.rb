7dcc2d7cde56f948a5dedba07d22746494cf32ec
Mathias Dalheimer <md@gonium.net>
2010-10-22 15:23:35 +0200

    deprecreated old daemon

--- lib/msg-chumby-daemon.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'      
-require 'flukso'
-require 'msg-chumby-daemon/http-xml-server.rb'
-require 'msg-chumby-daemon/reading-cache.rb'
-require 'msg-chumby-daemon/reading-importer.rb'
-
